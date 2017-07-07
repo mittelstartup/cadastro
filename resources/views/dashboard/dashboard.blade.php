7fc111128c2e168aa9652154660bcc9fb8079392
Jordan Bragon <jordan@organize.net.br>
2017-07-07 14:10:48 -0300

    Colocado o link para a home

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -39,19 +39,23 @@
 <!-- Navigation -->
 
 <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
-    <a class="navbar-brand page-scroll" href="https://mittelestagios.com/#page-top">Mittel</a>
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header page-scroll">
-
-            <a class="page-scroll" href="#page-top"> <h2>Olá, {{Auth::user()->name}}</h2></a>
-
+        <div class="navbar-header">
+            <ul class="nav navbar-nav navbar-left">
+                <li>
+                    <a class="page-scroll" href="#page-top" style="background-color: inherit;"> <b>Olá, {{Auth::user()->name}}</b></a>
+                </li>
+            </ul>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li class="hidden">
                     <a href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="{{url('/')}}">Home</a>
                 </li>
                 <li>
                     <a class="page-scroll" href="#about">
