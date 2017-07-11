2a9900fef206cf308f260df58f0021e1d73827c4
Jordan Bragon <jordan@organize.net.br>
2017-07-11 11:46:24 -0300

    Faltou o link do home no ultimo commit

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -62,7 +62,9 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-
+                <li>
+                    <a class="page-scroll" href="{{url('/')}}">Home</a>
+                </li>
                 <li>
                     <a class="page-scroll" href="#about"
                        @if(count($user->comprovantematricula) == 0)
@@ -123,9 +125,7 @@
                 {{--<li class="hidden">--}}
                     {{--<a href="#page-top"></a>--}}
                 {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="page-scroll" href="{{url('/')}}">Home</a>--}}
-                {{--</li>--}}
+
 
 
 
